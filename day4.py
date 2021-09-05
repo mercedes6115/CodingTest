8046d5bc623b5c5877379a460a6112711f9b29db
qoqhs <bp6115@naver.com>
2021-09-05 23:42:37 +0900

    day3

---
+++ day4.py
@@ -0,0 +1,28 @@
+import sys
+'''
+a= int(input()) # 5-1번
+b=list(map(int,input().split()))
+
+print(min(b),max(b))
+
+
+array_list = [] # 5-2번
+for i in range (9):
+    array_list.append(int(sys.stdin.readline()))
+max1=max(array_list)
+print(max(array_list))
+print(array_list.index(max1)+1)
+
+
+
+
+a = int(input()) # 5-3번
+b = int(input())
+c = int(input())
+d = a*b*c
+str_slit = list(str(d))
+
+for i in range(10):
+    print(str_slit.count(str(i)))
+
+'''
